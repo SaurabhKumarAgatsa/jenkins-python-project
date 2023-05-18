2e6a404aba0257593d6693c92aaf01a1a77db7d4
Agatsa <agatsa@Agatsas-MacBook-Air.local>
2023-05-18 15:36:36 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask, request, jsonify, make_response
+
+app = Flask(__name__)
+
+@app.route('/api/hello')
+def doctors():
+    return jsonify({"message":"Success","status":200})
+
+if __name__ == '__main__':
+    app.run(debug=True, port='3333',host='0.0.0.0')
